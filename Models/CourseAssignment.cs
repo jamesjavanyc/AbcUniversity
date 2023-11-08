02db0b045cb9369a59bbeb7d06207fa0b97931cb
jamesjavanyc` <1931736857@qq.com>
2023-11-07 22:23:15 -0500

    Add project files.

---
+++ Models/CourseAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class CourseAssignment
+    {
+        /* Keys */
+        public int InstructorID { get; set; }
+        public int CourseID { get; set; }
+
+        /* Navigation */
+        public Instructor Instructor { get; set; }
+        public Course Course { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        public string FirstMidName { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Enrollment Date")]
+        public DateTime EnrollmentDate { get; set; }
+
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstMidName;
+            }
+        }
+
+
+        /* Navigation */
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
